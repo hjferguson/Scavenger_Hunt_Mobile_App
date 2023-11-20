888932726bc4799a3fa9b01d88494fe4ffcf2971
OwenB-jng <owenbeattie1@gmail.com>
2023-11-19 20:05:30 -0500

    CompletedHuntsActivity now has a return capability to MainActivity

--- app/src/main/java/ca/gbc/scavengerhunt/CompletedHuntsActivity.java
+++ app/src/main/java/ca/gbc/scavengerhunt/CompletedHuntsActivity.java
@@ -2,7 +2,10 @@ package ca.gbc.scavengerhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CompletedHuntsActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class CompletedHuntsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_completed_hunts);
+
+
+        // Relates to returning to MainActivity
+        ImageButton returnBtn = findViewById(R.id.mainPageReturn);
+        returnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    public void onMainPageReturnClick(View view) {
+        finish();
     }
 }
