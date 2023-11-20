73af4c630a1ce636b17495e4114f79b7406944d0
RaleighD <raleigh.b.desmond@gmail.com>
2023-11-19 20:20:26 -0500

    Created achievements activity. Needs back button

--- app/src/main/java/ca/gbc/scavengerhunt/MainActivity.java
+++ app/src/main/java/ca/gbc/scavengerhunt/MainActivity.java
@@ -2,7 +2,11 @@ package ca.gbc.scavengerhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton startButton = findViewById(R.id.start_button);
+        ImageButton achieveButton = findViewById(R.id.achieve_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PoiSelectionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        achieveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AchieveActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }
