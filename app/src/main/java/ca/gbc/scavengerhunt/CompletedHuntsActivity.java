d957e10f1451c69c527b7b2075099b33e46f0dce
OwenB-jng <owenbeattie1@gmail.com>
2023-12-03 16:25:46 -0500

    fixed the view CompletedHuntsActivity button

--- app/src/main/java/ca/gbc/scavengerhunt/CompletedHuntsActivity.java
+++ app/src/main/java/ca/gbc/scavengerhunt/CompletedHuntsActivity.java
@@ -11,16 +11,18 @@ import android.widget.ImageButton;
 import ca.gbc.scavengerhunt.POI.PointOfInterest;
 
 public class CompletedHuntsActivity extends AppCompatActivity {
-
+    PointOfInterest poi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_completed_hunts);
-
-        //get poi from previous screen
-        PointOfInterest poi = (PointOfInterest) getIntent().getSerializableExtra("POI_DATA");
-        //this is a test, remove when done...
-        System.out.println("On the completed hunts... The title: " + poi.getName() + " and the desc: " + poi.getDescription());
+        poi = (PointOfInterest) getIntent().getSerializableExtra("POI_DATA");
+        if (poi != null){
+            //get poi from previous screen
+            PointOfInterest poi = (PointOfInterest) getIntent().getSerializableExtra("POI_DATA");
+            //this is a test, remove when done...
+            System.out.println("On the completed hunts... The title: " + poi.getName() + " and the desc: " + poi.getDescription());
+        }
 
         // Relates to returning to MainActivity
         ImageButton returnBtn = findViewById(R.id.mainPageReturn);
